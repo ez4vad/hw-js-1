42c7212beb3f4a48f7583c5364702c6b9782722b
[vadym] <[itsvadikk@gmail.com]>
2024-07-27 09:15:40 +0200

    commit hw ready

---
+++ main.js
@@ -0,0 +1,24 @@
+const name = prompt("what is your name");
+alert("hello" + " " + name);
+
+const date = prompt("what is your date of births");
+const age = 2024 - date;
+alert("your age is" + " " + age);
+
+const quadrat = prompt("add side");
+let p = quadrat * 4;
+alert("perimeter is" + " " + p);
+
+
+const circle = prompt("add r");
+const s = (circle * circle) * 3.14;
+alert("s is" + " " + s);
+
+const distance = prompt("add a distance");
+const time = prompt("add a time");
+const speed = distance/time;
+alert("speed must be" + " " + speed);
+
+const dolor = prompt("give summe");
+const summe = dolor * 0.92;
+alert(summe);
